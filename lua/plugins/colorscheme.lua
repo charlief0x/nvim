007d4d431ae7855651858d9ad123f9a9dfc540b3
Matthew Cerha <m.cerha@fetchrewards.com>
2026-05-21 12:24:41 -0500

    Add dracula-pro-nvim colorscheme, lazyvim extras, and lockfile

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "charlief0x/dracula-pro-nvim",
+    lazy = false,
+    priority = 1000,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "dracula_pro",
+    },
+  },
+}
